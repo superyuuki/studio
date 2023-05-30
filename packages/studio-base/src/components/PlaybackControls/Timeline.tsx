2e9c0a2e9c3ac17e3f5f74e7ae877b9cf73df076
Andrew Carr <andrew@foxglove.dev>
2023-05-30 12:43:25 +1000

    remove unused import

--- packages/studio-base/src/components/PlaybackControls/Timeline.tsx
+++ packages/studio-base/src/components/PlaybackControls/Timeline.tsx
@@ -2,7 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { Divider, Typography, useTheme } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 import { FzfResultItem } from "fzf";
 import { useCallback, useMemo, Dispatch, SetStateAction } from "react";
 import tc, { ColorInput } from "tinycolor2";
