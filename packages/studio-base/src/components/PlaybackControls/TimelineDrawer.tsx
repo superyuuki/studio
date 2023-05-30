7ad4be6eda3e7d35e4287dd434528c0b7b089b7e
Andrew Carr <andrew@foxglove.dev>
2023-05-30 11:19:24 +1000

    Separate components into files

--- packages/studio-base/src/components/PlaybackControls/TimelineDrawer.tsx
+++ packages/studio-base/src/components/PlaybackControls/TimelineDrawer.tsx
@@ -3,53 +3,25 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { DismissCircle12Filled, Search12Filled } from "@fluentui/react-icons";
-import { Fade, IconButton, Link, TextField, Tooltip, Typography, useTheme } from "@mui/material";
+import { IconButton, Link, TextField } from "@mui/material";
 import { Instance } from "@popperjs/core";
 import { Fzf, FzfResultItem } from "fzf";
 import { clamp, orderBy } from "lodash";
-import { useCallback, useRef, useState, useMemo, useEffect, Dispatch, SetStateAction } from "react";
-import { useLatest } from "react-use";
-import tc, { ColorInput } from "tinycolor2";
+import { useCallback, useRef, useState, useMemo } from "react";
 import { makeStyles } from "tss-react/mui";
-import { v4 as uuidv4 } from "uuid";
 
-import {
-  subtract as subtractTimes,
-  add as addTimes,
-  toSec,
-  fromSec,
-  Time,
-} from "@foxglove/rostime";
-import AutoSizingCanvas from "@foxglove/studio-base/components/AutoSizingCanvas";
+import { Time } from "@foxglove/rostime";
 import EmptyState from "@foxglove/studio-base/components/EmptyState";
-import { FzfHighlightChars } from "@foxglove/studio-base/components/FzfHighlightChars";
-import useMessagesByPath from "@foxglove/studio-base/components/MessagePathSyntax/useMessagesByPath";
 import {
   MessagePipelineContext,
   useMessagePipeline,
 } from "@foxglove/studio-base/components/MessagePipeline";
-import PlaybackBarHoverTicks from "@foxglove/studio-base/components/PlaybackControls/PlaybackBarHoverTicks";
-import { PlaybackControlsTooltipContent } from "@foxglove/studio-base/components/PlaybackControls/PlaybackControlsTooltipContent";
-import Slider from "@foxglove/studio-base/components/PlaybackControls/Slider";
+import { Timeline } from "@foxglove/studio-base/components/PlaybackControls/Timeline";
 import Stack from "@foxglove/studio-base/components/Stack";
-import {
-  useClearHoverValue,
-  useSetHoverValue,
-} from "@foxglove/studio-base/context/TimelineInteractionStateContext";
 import { PlayerPresence, TopicStats } from "@foxglove/studio-base/players/types";
 import { Topic } from "@foxglove/studio-base/src/players/types";
-import { expandedLineColors } from "@foxglove/studio-base/util/plotColors";
-import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 
 const useStyles = makeStyles()((theme) => ({
-  marker: {
-    backgroundColor: theme.palette.action.active,
-    position: "absolute",
-    height: "100%",
-    borderRadius: 1,
-    width: 2,
-    zIndex: theme.zIndex.appBar,
-  },
   root: {
     position: "relative",
     borderTop: `1px solid ${theme.palette.divider}`,
@@ -77,26 +49,8 @@ const useStyles = makeStyles()((theme) => ({
   },
 }));
 
-const ROW_HEIGHT = 48;
-
 const DRAWER_HEIGHT_MIN = 100;
 const DRAWER_HEIGHT_MAX = 2044;
-
-enum schemaMapping {
-  "diagnostic_msgs/DiagnosticArray" = 0,
-  "foxglove_msgs/ImageMarkerArray" = 1,
-  "geometry_msgs/PoseStamped" = 2,
-  "nav_msgs/OccupancyGrid" = 3,
-  "nav_msgs/Odometry" = 3,
-  "sensor_msgs/CameraInfo" = 4,
-  "sensor_msgs/CompressedImage" = 4,
-  "sensor_msgs/Imu" = 4,
-  "sensor_msgs/NavSatFix" = 4,
-  "sensor_msgs/PointCloud2" = 4,
-  "tf2_msgs/TFMessage" = 5,
-  "visualization_msgs/ImageMarker" = 6,
-  "visualization_msgs/MarkerArray" = 6,
-}
 
 type TopicWithStats = Topic & Partial<TopicStats>;
 
@@ -110,16 +64,9 @@ const topicToFzfResult = (item: TopicWithStats) =>
   } as FzfResultItem<TopicWithStats>);
 
 const selectSortedTopics = ({ sortedTopics }: MessagePipelineContext) => sortedTopics;
-const selectStartTime = (ctx: MessagePipelineContext) => ctx.playerState.activeData?.startTime;
-const selectCurrentTime = (ctx: MessagePipelineContext) => ctx.playerState.activeData?.currentTime;
-const selectEndTime = (ctx: MessagePipelineContext) => ctx.playerState.activeData?.endTime;
 const selectPresence = (ctx: MessagePipelineContext) => ctx.playerState.presence;
 
-type Props = {
-  onSeek: (seekTo: Time) => void;
-};
-
-export function TimelineDrawer(props: Props): JSX.Element {
+export function TimelineDrawer(props: { onSeek: (seekTo: Time) => void }): JSX.Element {
   const { onSeek } = props;
   const { classes } = useStyles();
 
@@ -246,290 +193,5 @@ export function TimelineDrawer(props: Props): JSX.Element {
         </EmptyState>
       )}
     </Stack>
-  );
-}
-
-export function TimelineScrubber({
-  height,
-  onSeek,
-  hoverStamp,
-  setHoverStamp,
-  drawerWidth,
-}: {
-  height: number;
-  drawerWidth: number;
-  hoverStamp?: Time;
-  setHoverStamp: Dispatch<SetStateAction<Time | undefined>>;
-  onSeek: (seekTo: Time) => void;
-}): JSX.Element {
-  const { classes } = useStyles();
-  const [hoverComponentId] = useState<string>(() => uuidv4());
-  const hoverElRef = useRef<HTMLDivElement>(ReactNull);
-
-  const startTime = useMessagePipeline(selectStartTime);
-  const currentTime = useMessagePipeline(selectCurrentTime);
-  const endTime = useMessagePipeline(selectEndTime);
-
-  const setHoverValue = useSetHoverValue();
-
-  const latestStartTime = useLatest(startTime);
-  const latestEndTime = useLatest(endTime);
-
-  const onChange = useCallback(
-    (fraction: number) => {
-      if (!latestStartTime.current || !latestEndTime.current) {
-        return;
-      }
-      onSeek(
-        addTimes(
-          latestStartTime.current,
-          fromSec(fraction * toSec(subtractTimes(latestEndTime.current, latestStartTime.current))),
-        ),
-      );
-    },
-    [onSeek, latestEndTime, latestStartTime],
-  );
-
-  const clearHoverValue = useClearHoverValue();
-
-  const onHoverOut = useCallback(() => {
-    clearHoverValue(hoverComponentId);
-  }, [clearHoverValue, hoverComponentId]);
-
-  // Clean up the hover value when we are unmounted -- important for storybook.
-  useEffect(() => onHoverOut, [onHoverOut]);
-
-  const onHoverOver = useCallback(
-    (fraction: number) => {
-      if (!latestStartTime.current || !latestEndTime.current || hoverElRef.current == undefined) {
-        return;
-      }
-      const duration = toSec(subtractTimes(latestEndTime.current, latestStartTime.current));
-      const timeFromStart = fromSec(fraction * duration);
-      setHoverStamp(addTimes(latestStartTime.current, timeFromStart));
-      setHoverValue({
-        componentId: hoverComponentId,
-        type: "PLAYBACK_SECONDS",
-        value: toSec(timeFromStart),
-      });
-    },
-    [hoverComponentId, latestEndTime, latestStartTime, setHoverStamp, setHoverValue],
-  );
-
-  const renderSlider = useCallback(
-    (val?: number) => {
-      if (val == undefined) {
-        return undefined;
-      }
-      return <div className={classes.marker} style={{ left: `${val * 100}%` }} />;
-    },
-    [classes.marker],
-  );
-
-  const min = startTime && toSec(startTime);
-  const max = endTime && toSec(endTime);
-  const fraction =
-    currentTime && startTime && endTime
-      ? toSec(subtractTimes(currentTime, startTime)) / toSec(subtractTimes(endTime, startTime))
-      : undefined;
-
-  const popperRef = React.useRef<Instance>(ReactNull);
-
-  const positionRef = React.useRef({ x: 0, y: 0 });
-
-  const handlePointerMove = (event: React.PointerEvent) => {
-    positionRef.current = { x: event.clientX, y: event.clientY };
-
-    if (popperRef.current != undefined) {
-      void popperRef.current.update();
-    }
-  };
-
-  return (
-    <Tooltip
-      title={hoverStamp != undefined ? <PlaybackControlsTooltipContent stamp={hoverStamp} /> : ""}
-      placement="top"
-      disableInteractive
-      TransitionComponent={Fade}
-      TransitionProps={{ timeout: 0 }}
-      PopperProps={{
-        popperRef,
-        modifiers: [
-          {
-            name: "computeStyles",
-            options: {
-              gpuAcceleration: false, // Fixes hairline seam on arrow in chrome.
-            },
-          },
-          {
-            name: "offset",
-            options: {
-              // Offset popper to hug the track better.
-              offset: [0, 0],
-            },
-          },
-        ],
-        anchorEl: {
-          getBoundingClientRect: () => {
-            return new DOMRect(
-              positionRef.current.x,
-              hoverElRef.current?.getBoundingClientRect().y ?? 0,
-              0,
-              0,
-            );
-          },
-        },
-      }}
-    >
-      <Stack
-        position="absolute"
-        flex="auto"
-        style={{ top: 0, right: 0, bottom: 0, left: drawerWidth, height, minHeight: "100%" }}
-        ref={hoverElRef}
-        onPointerMove={handlePointerMove}
-      >
-        <Slider
-          disabled={min == undefined || max == undefined}
-          fraction={fraction}
-          onHoverOver={onHoverOver}
-          onHoverOut={onHoverOut}
-          onChange={onChange}
-          renderSlider={renderSlider}
-        />
-        <PlaybackBarHoverTicks fullHeight componentId={hoverComponentId} />
-      </Stack>
-    </Tooltip>
-  );
-}
-
-export function Timeline({
-  topics = [],
-  hoverStamp,
-  setHoverStamp,
-  onSeek,
-}: {
-  topics?: FzfResultItem<Topic>[];
-  hoverStamp?: Time;
-  setHoverStamp: Dispatch<SetStateAction<Time | undefined>>;
-  onSeek: (seekTo: Time) => void;
-}): JSX.Element {
-  const theme = useTheme();
-
-  const pathStrings = useMemo(() => topics.map(({ item: { name } }) => name), [topics]);
-
-  const startTime = useMessagePipeline(selectStartTime);
-  const endTime = useMessagePipeline(selectEndTime);
-  const duration = toSec(subtractTimes(endTime!, startTime!));
-
-  const itemsByPath = useMessagesByPath(pathStrings);
-
-  const drawCallback = useCallback(
-    (ctx: CanvasRenderingContext2D, width: number, _height: number, _canvas: HTMLCanvasElement) => {
-      const augmentColor = (color: ColorInput) => {
-        const colorMode = theme.palette.mode === "dark" ? "darken" : "lighten";
-        const threshold = theme.palette.mode === "dark" ? 40 : 20;
-
-        return tc(color)[colorMode](threshold).toString();
-      };
-
-      topics.map(({ item: topic }, idx) => {
-        ctx.fillStyle = augmentColor(expandedLineColors[schemaMapping[topic.schemaName]]!);
-        ctx.fillRect(0, ROW_HEIGHT * idx, width, ROW_HEIGHT);
-
-        const canvasOffsets: number[] | undefined = itemsByPath[topic.name]?.map(
-          ({ messageEvent }) =>
-            (toSec(subtractTimes(messageEvent.receiveTime, startTime!)) / duration) * width,
-        );
-
-        if (canvasOffsets != undefined) {
-          for (const x of canvasOffsets) {
-            ctx.beginPath();
-            ctx.strokeStyle = tc(expandedLineColors[schemaMapping[topic.schemaName]])
-              .setAlpha(1)
-              .toString();
-            ctx.moveTo(x, ROW_HEIGHT * idx);
-            ctx.lineTo(x, ROW_HEIGHT * idx + ROW_HEIGHT);
-            ctx.stroke();
-          }
-        }
-      });
-
-      topics.map((_, idx) => {
-        ctx.strokeStyle = theme.palette.background.default;
-        ctx.strokeRect(0, ROW_HEIGHT * idx, width, ROW_HEIGHT);
-      });
-
-      ctx.save();
-    },
-    [
-      duration,
-      itemsByPath,
-      startTime,
-      theme.palette.background.default,
-      theme.palette.mode,
-      topics,
-    ],
-  );
-
-  return (
-    <>
-      <Stack
-        direction="row"
-        fullWidth
-        flex="auto"
-        position="relative"
-        overflow="auto"
-        justifyContent="flex-start"
-      >
-        <div
-          style={{
-            inset: "0 0 auto 300px",
-            position: "absolute",
-            height: topics.length * ROW_HEIGHT,
-          }}
-        >
-          <AutoSizingCanvas draw={drawCallback} />
-        </div>
-
-        <TimelineScrubber
-          onSeek={onSeek}
-          hoverStamp={hoverStamp}
-          setHoverStamp={setHoverStamp}
-          drawerWidth={300}
-          height={topics.length * ROW_HEIGHT}
-        />
-
-        <div>
-          {topics.map(({ item: topic, positions }, idx) => (
-            <Stack
-              overflow="hidden"
-              flex="none"
-              key={`${idx}.${topic.name}`}
-              justifyContent="center"
-              padding={1}
-              style={{
-                width: 300,
-                borderBottom: `1px solid ${theme.palette.divider}`,
-                boxSizing: "border-box",
-                height: ROW_HEIGHT,
-              }}
-            >
-              <Typography variant="caption">
-                <FzfHighlightChars str={topic.name} indices={positions} />
-              </Typography>
-              <Typography variant="caption" fontFamily={fonts.MONOSPACE} color="text.secondary">
-                {topic.schemaName != undefined && (
-                  <FzfHighlightChars
-                    str={topic.schemaName}
-                    indices={positions}
-                    offset={topic.name.length + 1}
-                  />
-                )}
-              </Typography>
-            </Stack>
-          ))}
-        </div>
-      </Stack>
-    </>
   );
 }
