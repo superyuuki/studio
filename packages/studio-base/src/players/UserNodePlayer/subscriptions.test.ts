bbd7ef011f720c1fc0f8ab17bd0cf29fe30e5a7c
Sam Nosenzo <samnosenzo@gmail.com>
2023-10-03 16:31:06 -0400

    - add test and remove coalescing input subs

--- packages/studio-base/src/players/UserNodePlayer/subscriptions.test.ts
+++ packages/studio-base/src/players/UserNodePlayer/subscriptions.test.ts
@@ -156,4 +156,33 @@ describe("remapVirtualSubscriptions", () => {
       },
     ]);
   });
+
+  it("should not coalesce sliced input subscriptions and whole message input subscriptions across preload types", () => {
+    const subscriptions: SubscribePayload[] = [
+      {
+        topic: "/output",
+        preloadType: "full",
+      },
+      {
+        topic: "/input",
+        preloadType: "partial",
+        fields: ["one", "two"],
+      },
+    ];
+    const inputsByOutputTopic = {
+      "/output": ["/input"],
+    };
+    const expected = [
+      {
+        topic: "/input",
+        preloadType: "full",
+      },
+      {
+        topic: "/input",
+        preloadType: "partial",
+        fields: ["one", "two"],
+      },
+    ];
+    expect(call(subscriptions, inputsByOutputTopic)).toEqual(expected);
+  });
 });
