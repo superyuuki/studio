b8aa4df3cfc592409001527a081658e955097221
Caleb Foust <cfoust@sqweebloid.com>
2023-10-13 13:42:08 -0400

    feat: partial implementation of lttb

---
+++ packages/studio-base/src/components/TimeBasedChart/lttb.ts
@@ -0,0 +1,102 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import * as R from "ramda";
+
+type Point = [x: number, y: number];
+type IndexedPoint = [index: number, point: Point];
+
+const containsNaN = ([, [x, y]]: IndexedPoint): boolean => isNaN(x) || isNaN(y);
+
+/**
+ * Choose a subset of points from the provided dataset that retain its visual properties. The output of this algorithm is only well-defined for datasets where the x-axis is sorted and is plotted as a contiguous line.
+ *
+ * This is an implementation of the "Largest Triangle Three Buckets" algorithm as it appears in Sveinn Steinarsson's 2013 master's thesis of the same name.
+ * More information and other implementations: https://github.com/sveinn-steinarsson/flot-downsample
+ * The thesis: http://hdl.handle.net/1946/15343
+ * Some insight derived from this native JavaScript implementation (license MIT): https://github.com/joshcarr/largest-triangle-three-buckets.js/blob/master/lib/largest-triangle-three-buckets.js
+ */
+export function downsampleLTTB(
+  get: (index: number) => Point | undefined,
+  numPoints: number,
+  numBuckets: number,
+): number[] | undefined {
+  if (numBuckets >= numPoints || numBuckets === 0) {
+    return R.range(0, numPoints);
+  }
+
+  const bucketSize = (numPoints - 2) / (numBuckets - 2);
+
+  const getBucket = (index: number): [start: number, end: number] => [
+    Math.floor(index * bucketSize),
+    Math.floor((index + 1) * bucketSize),
+  ];
+
+  const getPoints = (start: number, end: number): IndexedPoint[] | undefined => {
+    const range = R.range(start, end);
+    const points = R.chain((i: number): IndexedPoint[] => {
+      const point = get(i);
+      return point != undefined ? [[i, point]] : [];
+    }, range);
+
+    if (points.length < range.length) {
+      return undefined;
+    }
+    return points;
+  };
+
+  let next: number = 0;
+  let points: number[] = [];
+  points.push(0);
+  for (const bucketIndex of R.range(0, bucketSize - 2)) {
+    const [bucketStart, bucketEnd] = getBucket(bucketIndex);
+    // First, get all of the points for this bucket so we can check for
+    // nullity and/or NaN
+    const bucketPoints = getPoints(bucketStart, bucketEnd);
+    if (bucketPoints == undefined) {
+      return undefined;
+    }
+
+    // Next, get the average of the following bucket
+    const [nextStart, nextEnd] = getBucket(bucketIndex + 1);
+    const avgPoints = getPoints(nextStart, Math.min(nextEnd, numPoints));
+    if (avgPoints == undefined) {
+      return undefined;
+    }
+
+    // Check all points under consideration for NaN. We use NaN to imply a
+    // break in the plot; this has to be given special treatment so that we
+    // downsample both parts of the plot separately.
+    const nanPoint = R.find(containsNaN, [...bucketPoints, ...avgPoints]);
+    if (nanPoint != undefined) {
+      const [nanIndex] = nanPoint;
+
+      // Attempt to add the last point before the NaN
+      if (nanIndex - 1 >= bucketStart) {
+        points.push(nanIndex - 1);
+      }
+
+      // If NaN comes at the very end, there's no point in continuing
+      const remainingPoints = numPoints - (nanIndex + 1);
+      if (remainingPoints <= 0) {
+        return points;
+      }
+
+      // Downsample the rest of the dataset separately
+      const rest = downsampleLTTB(
+        (index) => get(index + nanIndex + 1),
+        remainingPoints,
+        numBuckets - bucketIndex,
+      );
+      if (rest == undefined) {
+        return undefined;
+      }
+
+      return points.concat(rest);
+    }
+  }
+
+  points.push(numPoints - 1);
+  return points;
+}
